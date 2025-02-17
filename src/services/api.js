876c1f42f8ee1b4b4bba07445bed89024a018485
Patthanan Jaichuai <PatthananLT@gmail.com>
2025-02-18 03:13:22 +0700

    VITE_API_BASE_URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://pms-server-production-3ccc.up.railway.app/api";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
